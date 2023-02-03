2f8fe78cbeb6983e0d5e0dc6073a05d9d5f6365b
Adil <adilzhapar2002@gmail.com>
2023-02-03 17:09:02 +0600

    upd

--- w1/a.py
+++ w1/a.py
@@ -1,2 +1,32 @@
 print("hello alem")
 
+x = 5
+
+print(x)
+
+x = 'hello'
+
+print(type(x))
+
+y = 5.5
+
+print(type(y))
+
+print(x, y)
+
+print("odd")
+
+
+print("--------")
+a = int(input())
+b = int(input())
+c = int(input())
+print(a + b + c)
+
+print("--------")
+
+a = int(input("Input number for incrementing: "))
+# a = a + 1 same as a += 1
+a += 1
+a -= 3
+print(a)
